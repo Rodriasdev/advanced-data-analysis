219879b7e0e48e7a49a68b024b9d42ee2d671899
Rodriasd <veronrodrigo98@gmail.com>
2024-08-05 15:25:25 -0300

    added the logic for create the table

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import MySQLdb
+import sys
 from db import connectdb
 
 cursor, db = connectdb()
+
+
+try:
+    cursor.execute("DROP TABLE IF EXISTS EmployeePerformance")
+    cursor.execute("CREATE TABLE EmployeePerformance (id INT AUTO_INCREMENT PRIMARY KEY, employee_id INT, departament VARCHAR(255), performance_score FLOAT, years_with_company INT, salary FLOAT)")
+except MySQLdb.error as e:
+    print(e)
+    sys.exit(1)
